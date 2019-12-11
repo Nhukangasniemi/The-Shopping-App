d3405465301d729f51efd72d0250dbbd86cf74f4
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-12-11 11:30:08 +0200

    Setup Actions and Reducers

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {FlatList} from 'react-native'
+
+const ProductsOverviewScreen = () => {
+    return (
+        <FlatList/>
+    )
+}
+
+export default ProductsOverviewScreen
