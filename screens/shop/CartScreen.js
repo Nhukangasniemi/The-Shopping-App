beaffbe6df65789578a4b14e9524ae8019afb5d6
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-12-16 14:58:28 +0200

    Adding REMOVE_FROM_CART action and reducer

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,23 +1,58 @@
 import React from "react";
 import { StyleSheet, View, Text, FlatList, Button } from "react-native";
-import { useSelector } from "react-redux";
-import { useSelector } from "react-redux";
-import { useSelector } from "react-redux";
-import DefaultText from './../../components/DefaultText';
+import { useSelector, useDispatch } from "react-redux";
+import DefaultText from "./../../components/DefaultText";
 import Colors from "../../constants/Colors";
+import CartItem from "../../components/shop/CartItem";
+import { removeFromCart } from "../../store/actions/cart";
 
 const CartScreen = () => {
-  const cartTotalAmount = useSelector(state => state.cart.totalAmount);
+  const cartTotalAmount = useSelector(state =>
+    state.cart.totalAmount.toFixed(2)
+  );
+  const cartItems = useSelector(state => {
+    const transformedCartItems = [];
+    for (const key in state.cart.items) {
+      transformedCartItems.push({
+        productId: key,
+        productTitle: state.cart.items[key].productTitle,
+        productPrice: state.cart.items[key].productPrice,
+        quantity: state.cart.items[key].quantity,
+        sum: state.cart.items[key].sum
+      });
+    }
+    return transformedCartItems.sort((a, b) =>
+      a.productId > b.productId ? 1 : -1
+    );
+  });
+  const dispatch = useDispatch();
+
   return (
     <View style={styles.screen}>
       <View style={styles.summary}>
         <DefaultText style={styles.summaryText}>
-          Total: <DefaultText style={styles.amount}>${cartTotalAmount}</DefaultText>
+          Total:{" "}
+          <DefaultText style={styles.amount}>${cartTotalAmount}</DefaultText>
         </DefaultText>
-        <Button title="Order Now" />
+        <Button
+          color={Colors.accent}
+          title="Order Now"
+          disabled={cartItems.length === 0}
+        />
       </View>
       <View>
-        <DefaultText>CART ITEMS</DefaultText>
+        <FlatList
+          data={cartItems}
+          keyExtractor={item => item.productId}
+          renderItem={({ item }) => (
+            <CartItem
+              quantity={item.quantity}
+              title={item.productTitle}
+              amount={item.sum}
+              onRemove={() => dispatch(removeFromCart(item.productId))}
+            />
+          )}
+        />
       </View>
     </View>
   );
@@ -28,9 +63,9 @@ const styles = StyleSheet.create({
     margin: 20
   },
   summary: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     marginBottom: 20,
     padding: 10,
     shadowColor: "black",
@@ -39,14 +74,14 @@ const styles = StyleSheet.create({
     shadowRadius: 8,
     elevation: 5,
     borderRadius: 10,
-    backgroundColor: "white",
+    backgroundColor: "white"
   },
   summaryText: {
     fontSize: 18,
-    fontFamily: 'open-sans-bold'
+    fontFamily: "open-sans-bold"
   },
   amount: {
-    color: Colors.accent
+    color: Colors.primary
   }
 });
 
