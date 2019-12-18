13852b2d18957cdbe2f1526bf50cd88482f490e2
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2019-12-18 09:31:19 +0200

    Create OrderItem look and import momentjs

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,13 +3,16 @@ import { FlatList, Text, Platform } from "react-native";
 import { useSelector } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
 
 export const OrdersScreen = props => {
   const orders = useSelector(state => state.orders.orders);
   return (
     <FlatList
       data={orders}
-      renderItem={({ item }) => <Text>{item.totalAmount}</Text>}
+      renderItem={({ item }) => (
+        <OrderItem amount={item.totalAmount} date={item.readableDate} />
+      )}
     />
   );
 };
