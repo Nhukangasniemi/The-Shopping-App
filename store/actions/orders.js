ab955c4f4a1498802d4a428eb4c3cb23f4d39085
nhukangasniemi <nhu.kangasniemi@bilot.fi>
2020-01-03 15:14:03 +0200

    Storing Orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,28 +1,34 @@
 export const ADD_ORDER = "ADD_ORDER";
 
 export const addOrder = (cartItems, totalAmount) => async dispatch => {
+    const date = new Date()
   const response = await fetch(
-    "https://the-shopping-app-7385a.firebaseio.com/products.json",
+    "https://the-shopping-app-7385a.firebaseio.com/orders/u1.json",
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        title,
-        description,
-        imageUrl,
-        price
+        cartItems,
+        totalAmount,
+        date: date.toISOString()
       })
     }
   );
+
+  if (!response.ok) {
+    throw new Error("Something went wrong!");
+  }
 
   const resData = await response.json();
   dispatch({
     type: ADD_ORDER,
     orderData: {
+      id: resData.name,
       items: cartItems,
-      amount: totalAmount
+      amount: totalAmount,
+      date: date
     }
   });
 };
